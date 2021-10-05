8d34e1b3f7c78da31f9d128f80746396faf419b6
Akhmad Salafudin <axmad386@gmail.com>
2021-10-05 16:43:21 +0700

    chore: add tailwindcss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '$lib/style/app.css';
+</script>
+
+<slot />
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+      './src/lib/component/**/*.svelte',
+      './src/routes/**/*.svelte'
+  ],
+  theme: {
+      extend: {
+      }
+  }
+}
