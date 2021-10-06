578abdf54c84ba26be4100241c6038cb85c55504
yudian nanda <yudiannanwebdev@gmail.com>
2021-10-06 10:11:24 +0700

    frontend page home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-      './src/lib/component/**/*.svelte',
-      './src/routes/**/*.svelte'
-  ],
-  theme: {
-      extend: {
-      }
-  }
-}
+	mode: 'jit',
+	purge: ['./src/lib/component/**/*.svelte', './src/routes/**/*.svelte'],
+	theme: {
+		extend: {
+			fontFamily: {
+				lato: ['Lato', 'sans-seri'],
+				arab: ['Scheherazade New', 'serif']
+			},
+			fontSize: { text24: '24px' }
+		}
+	}
+};
