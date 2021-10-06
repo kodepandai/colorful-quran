6132163507b26a268418645060e50351b2c10fda
Akhmad Salafudin <axmad386@gmail.com>
2021-10-06 10:38:21 +0700

    feat: font arab Scheherazade New

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
+	import FontLoader from '$component/support/FontLoader.svelte';
 	import '$lib/style/app.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet" />
+</svelte:head>
+
 <slot />
+<FontLoader />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
       extend: {
+        fontFamily: {
+          arab: ['var(--font-arab)', 'serif']
+        }
       }
   }
 }
