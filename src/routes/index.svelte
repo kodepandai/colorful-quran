4901c8d919d0beaabe2b6f6d1f542cef19cce01a
yudianwebdev <69705828+yudianwebdev@users.noreply.github.com>
2021-10-06 11:10:12 +0700

    feat: styling home page (#1)
    * frontend page home
    
    * git push

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import list from '$db/kemenag/list.json';
+	import SurahId from './[source]/surah/[surah_id].svelte';
 </script>
 
-<h1>Al Qur'an</h1>
-<div class="bg-green-300">
-	<h2>List Surah</h2>
-	<ul class="list-inside">
-		{#each list as surah}
-			<li class="list-item list-decimal">
-				<a href={`/kemenag/surah/${surah.id}`}>
+<h1 class="text-7xl text-center font-lato">Al Qur'an</h1>
+<div class=" xl:w-6/12 mx-auto px-4">
+	{#each list as surah}
+		<div class="p-2 ">
+			<div
+				class=" p-3 w-full hover:bg-green-300 bg-gray-400 rounded-full  text-center flex-row grid"
+			>
+				<a class=" font-arab text-2xl" href={`/kemenag/surah/${surah.id}`}>
+					{surah.surat_text}
+				</a>
+				<a class="text-xs" href={`/kemenag/surah/${surah.id}`}>
 					{surah.surat_name}
 				</a>
-			</li>
-		{/each}
-	</ul>
+			</div>
+		</div>
+	{/each}
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,11 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-      './src/lib/component/**/*.svelte',
-      './src/routes/**/*.svelte'
-  ],
-  theme: {
-      extend: {
-        fontFamily: {
-          arab: ['var(--font-arab)', 'serif']
-        }
-      }
-  }
-}
+	mode: 'jit',
+	purge: ['./src/lib/component/**/*.svelte', './src/routes/**/*.svelte'],
+	theme: {
+		extend: {
+			fontFamily: {
+				arab: ['var(--font-arab)', 'serif']
+			}
+		}
+	}
+};
