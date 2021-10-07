a6e819461d797284e9c4352a2d2951bee0723646
Akhmad Salafudin <axmad386@gmail.com>
2021-10-07 10:11:03 +0700

    feat: tajweed surah 1 kemenag version (#3)
    * wip: tajweed mapping
    
    * feat: tajweed surah 1 kemenag version

--- src/routes/[source]/surah/[surah_id].svelte
+++ src/routes/[source]/surah/[surah_id].svelte
@@ -1,15 +1,13 @@
 <script context="module" lang="ts">
-	import type { IAya } from '$contract/surah';
-
+	import type { IAya, ITajweed } from '$contract/surah';
 	import type { Load } from '@sveltejs/kit';
-	let surah: IAya[];
 
 	const getSurah = async (source, surah_id) => {
-		surah = (await import(`../../../db/${source}/surah/${surah_id}.json`)).default;
+		return (await import(`../../../db/${source}/surah/${surah_id}.json`)).default as IAya[];
 	};
 
 	export const load: Load = async ({ page }) => {
-		await getSurah(page.params.source, page.params.surah_id);
+		const surah = await getSurah(page.params.source, page.params.surah_id);
 		return {
 			status: 200,
 			props: {
@@ -19,13 +17,54 @@
 	};
 </script>
 
-{#each surah as aya}
-<div class="p-2">
-	<div class="font-arab p-3 bg-gray-400 rounded-md" style="direction: rtl;">
-		{`${aya.aya_text.slice}`}
-	</div>
-	<div class="font-arab mt-2">
-		{`${aya.aya_number}. ${aya.translation_aya_text}`}
+<script lang="ts">
+	export let surah: IAya[];
+	surah.map((aya) => {
+		let sliceAya: ITajweed[] = [
+			{
+				class: '',
+				start: 0,
+				end: 0
+			}
+		];
+		aya.tajweed?.forEach((t) => {
+			const lastSlice = sliceAya[sliceAya.length - 1];
+			if (lastSlice.end < t.start) {
+				sliceAya.push({
+					class: '',
+					start: lastSlice.end,
+					end: t.start
+				});
+				sliceAya.push(t);
+			} else {
+				sliceAya.push(t);
+			}
+		});
+		sliceAya.push({
+			class: '',
+			start: sliceAya[sliceAya.length - 1].end,
+			end: aya.aya_text.length
+		});
+		sliceAya.shift();
+		aya.tajweed = sliceAya;
+		console.log(aya);
+		return aya;
+	});
+</script>
+
+<div class="h-full w-full bg-gray-200">
+	<div class="container mx-auto p-4">
+		{#each surah as aya}
+			<div class="font-arab p-3 shadow rounded-md mb-4 bg-white text-xl">
+				{#each aya.tajweed as tajweed}
+					<i class={tajweed.class}>
+						{aya.aya_text.slice(tajweed.start, tajweed.end)}
+					</i>
+				{/each}
+				<p class="font-sans text-sm mt-2 border-t pt-2" style="direction: ltr;">
+					{aya.aya_number}. {aya.translation_aya_text}
+				</p>
+			</div>
+		{/each}
 	</div>
 </div>
-{/each}
