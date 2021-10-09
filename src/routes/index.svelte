0b53c986e90452b560c1bcdc227fae4ab1ba399c
tio <tio@HP-EliteBook-820-G4-9c47e3b7.localdomain>
2021-10-10 02:53:52 +0700

    change image quran and add function search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,21 +2,27 @@
 	import list from '$db/kemenag/list.json';
 	import IconSearch from '../icon/search.svelte';
 	import IconBookOpen from '../icon/book-open.svelte';
+
+	let dataView = list;
+	let querySearch = '';
+	let dataFiltered = [];
+	$: {
+		dataFiltered = dataView.filter((data) => {
+			if (querySearch != null) {
+				return data.surat_name.toLocaleLowerCase().includes(querySearch);
+			}
+		});
+	}
 </script>
 
 <div class="sm:w-1/2 flex flex-col p-4 mx-auto">
-	<div class="flex flex-row py-2">
-		<div class="w-11/12 pl-4">
-			<span class="font-bold text-indigo-900 text-xl">Colorful Quran</span>
-		</div>
-		<div class="px-2">
-			<IconSearch fill="#000" width={20} height={20} />
-		</div>
+	<div class="flex justify-center py-2">
+		<span class="font-bold text-indigo-900 text-xl">Colorful Quran</span>
 	</div>
 	<div
-		class=" from-blue-500 to-indigo-700 bg-gradient-to-br rounded-xl px-4 py-8 mb-4 flex flex-row"
+		class=" from-blue-500 to-indigo-700 bg-gradient-to-br rounded-xl pl-4 pt-6 mb-2 flex flex-row"
 	>
-		<div class="w-9/12 flex flex-col">
+		<div class="lg:w-9/12 w-8/12 flex flex-col pb-6">
 			<span class="text-white text-sm mb-2">Assalamualaikum</span>
 			<div class="flex flex-row mb-2">
 				<IconBookOpen fill="#fff" width={15} height={15} />
@@ -24,11 +30,22 @@
 			</div>
 			<span class="text-white text-xs">tidak ada surat yang disimpan</span>
 		</div>
-		<div class="px-2 w-3/12">
-			<IconBookOpen width={70} height={70} />
+		<div class="lg:w-3/12 w-4/12 justify-center flex">
+			<img src="icon.png" alt="" class="lg:h-24" />
 		</div>
 	</div>
-	{#each list as surah}
+	<div class="bg-gray-200 rounded-xl flex flex-row py-2 px-4">
+		<input
+			bind:value={querySearch}
+			type="text"
+			placeholder="Search Surah"
+			class="bg-transparent focus:outline-none w-11/12"
+		/>
+		<div class="px-2 align-middle flex" style="padding-top: 0.2rem;">
+			<IconSearch fill="#000" width={20} height={20} />
+		</div>
+	</div>
+	{#each dataFiltered as surah}
 		<a class="flex flex-row p-4 mb-2 justify-between border-b" href={`/kemenag/surah/${surah.id}`}>
 			<div class="flex">
 				<div class="block pt-1">
