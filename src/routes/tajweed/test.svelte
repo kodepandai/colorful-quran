e1486d802931f517e737fd770f3aedaecea9ac85
Akhmad Salafudin <axmad386@gmail.com>
2021-10-13 23:15:32 +0700

    feat: idgham-bighunnah rule finder

---
+++ src/routes/tajweed/test.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Ghunnah from '$support/tajweed/kemenag/rule/Ghunnah';
+	import surah from '$db/kemenag/surah/2.json';
+	import GenerateRule from '$support/tajweed/GenerateRule';
+	import IdghamBighunnah from '$support/tajweed/kemenag/rule/IdghamBighunnah';
+
+	const testAya = surah[4].aya_text;
+	GenerateRule(testAya, [Ghunnah, IdghamBighunnah]).then((result) => {
+		console.log(testAya, result);
+	});
+</script>
