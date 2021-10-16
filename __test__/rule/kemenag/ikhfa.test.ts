ba1a375395523978e3a84ccef0036b748f21e10c
Akhmad Salafudin <axmad386@gmail.com>
2021-10-16 10:07:21 +0700

    test: ikhfa finder test

---
+++ __test__/rule/kemenag/ikhfa.test.ts
@@ -0,0 +1,30 @@
+import surah2 from '$db/kemenag/surah/2.json';
+import Ikhfa from '$support/tajweed/kemenag/rule/Ikhfa';
+import GenerateRule from '$support/tajweed/GenerateRule';
+
+describe('Ikhfa finder kemenag version', () => {
+	it('cannot find ikhfa on surah 2 verse 2', async () => {
+		const result = await GenerateRule(surah2[1].aya_text, [Ikhfa]);
+		expect(result.length).toEqual(0);
+	});
+
+	it('can find ikhfa on surah 2 verse 4', async () => {
+		const result = await GenerateRule(surah2[3].aya_text, [Ikhfa]);
+		expect(result.length).toEqual(3);
+		expect(result[0]).toEqual({
+			class: 'ikhfa',
+			start: 35,
+			end: 39
+		});
+		expect(result[1]).toEqual({
+			class: 'ikhfa',
+			start: 60,
+			end: 64
+		});
+		expect(result[2]).toEqual({
+			class: 'ikhfa',
+			start: 69,
+			end: 74
+		});
+	});
+});
