46153f6aeed59725b9eaadba16153f9cef7aec41
sangvictim <sangvictim@gmail.com>
2021-10-22 17:03:01 +0700

    fix: conflict

--- src/component/Modal.svelte
+++ src/component/Modal.svelte
@@ -1,47 +1,25 @@
-<script>
+<script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	const dismissModal = () => {
+		dispatch('dismiss');
+	};
 </script>
 
-<div
-	class="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
-	id="modal-id"
->
+<div class="min-w-screen h-screen fixed flex justify-center inset-0 z-50">
 	<div class="absolute bg-black opacity-30 inset-0 z-0" />
-	<div
-		class="w-full transition-transform duration-500 ease-linear max-w-xs relative mx-auto my-auto rounded-xl shadow-lg bg-white"
-	>
-		<div class="px-3 float-right">
-			<Icon icon="ant-design:close-circle-filled" width="30" height="30" />
-		</div>
+	<div class="w-full max-w-xs relative m-auto rounded-xl bg-white">
+		<button class="p-1 float-right" on:click={dismissModal}>
+			<Icon icon="ant-design:close-circle" color="#383838" width="30" height="30" />
+		</button>
 		<!--content-->
 		<div class="p-4">
 			<!--body-->
 			<div class="flex flex-col">
 				<!-- conten is dinamic -->
-				<div class="border-b py-2 flex mb-3">
-					<div class="w-12 flex justify-center bg-blue-500">
-						<span>icon</span>
-					</div>
-					<span>Nama menu</span>
-				</div>
-				<div class="border-b py-2 flex mb-3">
-					<div class="w-12 flex justify-center bg-blue-500">
-						<span>icon</span>
-					</div>
-					<span>Nama menu</span>
-				</div>
-				<div class="border-b py-2 flex mb-3">
-					<div class="w-12 flex justify-center bg-blue-500">
-						<span>icon</span>
-					</div>
-					<span>Nama menu</span>
-				</div>
-				<div class="border-b py-2 flex mb-3">
-					<div class="w-12 flex justify-center bg-blue-500">
-						<span>icon</span>
-					</div>
-					<span>Nama menu</span>
-				</div>
+				<slot />
 				<!-- end conten -->
 			</div>
 		</div>
--- src/component/header.svelte
+++ src/component/header.svelte
@@ -2,6 +2,9 @@
 	import Icon from '@iconify/svelte';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import Modal from './Modal.svelte';
+
+	let showModal = false;
 </script>
 
 <div class="flex justify-between h-14 sticky top-0 bg-primary">
@@ -14,8 +17,57 @@
 		<h1 class="font-bold text-xl text-white">Colorful Quran</h1>
 	</div>
 	{#if $page.path != '/'}
-		<div class="h-full cursor-pointer items-center flex mr-4">
+		<div class="h-full cursor-pointer items-center flex mr-4" on:click={() => (showModal = true)}>
 			<Icon icon="clarity:cog-line" width="30" height="30" color="#fff" />
 		</div>
 	{/if}
 </div>
+
+{#if showModal}
+	<Modal on:dismiss={() => (showModal = false)}>
+		<div class="border-b py-2 flex mb-3 cursor-pointer">
+			<div class="w-12 flex justify-center">
+				<Icon icon="fluent:dark-theme-24-filled" width="20" height="20" />
+			</div>
+			<span>Tema gelap / terang</span>
+		</div>
+		<div class="border-b py-2 flex mb-3 cursor-pointer">
+			<div class="w-12 flex justify-center">
+				<Icon icon="ant-design:copy-outlined" width="20" height="20" />
+			</div>
+			<span>Salin tautan surah ini</span>
+		</div>
+		<div class="border-b py-2 flex mb-3 cursor-pointer">
+			<div class="w-12 flex justify-center">
+				<Icon icon="emojione-monotone:bookmark" width="20" height="20" />
+			</div>
+			<span>Tandai surah ini</span>
+		</div>
+		<div class="border-b py-2 flex mb-3 cursor-pointer">
+			<div class="w-12 flex justify-center">
+				<Icon icon="codicon:book" width="20" height="20" />
+			</div>
+			<span>Mode Buku / per ayat</span>
+		</div>
+		<div class="border-b py-2 flex mb-3">
+			<div class="w-12 flex justify-center">
+				<Icon icon="bx:bx-font-size" width="20" height="20" />
+			</div>
+			<div class="block">
+				<span>Ukuran Ayat</span>
+				<input type="range" min="1" max="100" value="12" class="w-full" />
+				<span class="text-xs text-graySecond">12 px</span>
+			</div>
+		</div>
+		<div class="border-b py-2 flex mb-3 cursor-pointer">
+			<div class="w-12 flex justify-center">
+				<Icon icon="bx:bx-font-size" width="20" height="20" />
+			</div>
+			<div class="block">
+				<span>Ukuran Terjemahan</span>
+				<input type="range" min="1" max="100" value="12" class="w-full" />
+				<span class="text-xs text-graySecond">12 px</span>
+			</div>
+		</div>
+	</Modal>
+{/if}
