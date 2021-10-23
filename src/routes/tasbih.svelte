048d77e206f75b0aa03c57cac4dcd0722e67b97f
Akhmad Salafudin <axmad386@gmail.com>
2021-10-23 11:15:43 +0700

    fix: tasbih responsive

--- src/routes/tasbih.svelte
+++ src/routes/tasbih.svelte
@@ -1,26 +1,24 @@
 <script lang="ts">
-	import Icon from '@iconify/svelte';
-
 	export let count = 0;
 </script>
 
-<div class="sm:w-1/2 flex flex-col p-4 mx-auto">
-	<div class="text-center mb-2">
-		<span class="font-bold text-indigo-900 text-xl">Colorful Quran</span>
-	</div>
-	<div class="flex justify-center tasbihconter relative">
-		<img src="tasbih.png" alt="" class="sm:h-full sm:w-full" />
-		<div class="absolute justify-center items-center flex countconter  top-0 right-0 left-0 w-full">
-			<p class="text-3xl">{count}</p>
-		</div>
-		<div
-			on:click={() => (count = 0)}
-			class="absolute justify-center items-center flex top165 text-white"
-		>
-			<p>Reset</p>
-		</div>
-		<div on:click={() => (count += 1)} class="tmblcount bg-indigo-900">
-			<img src="counter.svg" alt="Count" class="h-full w-full p-3 " />
+<div class="flex h-screen -mt-14 pt-14 z-0">
+	<div class="h-full w-full justify-center items-center flex">
+		<div class="w-full h-full relative" style="max-width: 343px; max-height: 343px;">
+			<img src="tasbih.png" alt="" class="h-full w-full" />
+			<p class="text-3xl absolute top-0 mt-20 text-center w-full">{count}</p>
+			<button
+				on:click={() => (count = 0)}
+				class="absolute left-1/2 -translate-x-1/2 top-0 mt-44 text-white"
+			>
+				<p>Reset</p>
+			</button>
+			<button
+				on:click={() => (count += 1)}
+				class="w-20 h-20 rounded-full bg-indigo-900 absolute left-1/2 -translate-x-1/2 bottom-0 mb-10"
+			>
+				<img src="counter.svg" alt="Count" class="h-full w-full p-3 " />
+			</button>
 		</div>
 	</div>
 </div>
