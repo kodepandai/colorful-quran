b005e4870138eee03f65ff25b3a6cab51a8dfec1
Akhmad Salafudin <axmad386@gmail.com>
2021-10-29 16:00:19 +0700

    fix: madd jaiz and madd wajib start index (#55)
    * fix: madd wajib and madd jaiz start index
    
    * chore: generate tajweed with fixed madd jaiz and madd wajib

--- src/tajweed/kemenag/rule/MaddWajib.ts
+++ src/tajweed/kemenag/rule/MaddWajib.ts
@@ -8,20 +8,21 @@ import {
 	Maddah,
 	MaddahShort
 } from '$tajweed/kemenag/check/Char';
-import { GetNext } from '$tajweed/kemenag/check/Pointer';
+import { GetNext, GetPrevHuruf } from '$tajweed/kemenag/check/Pointer';
 
 const MaddWajib: RuleFinder = (ayaSplited) => {
 	return new Promise((resolve) => {
 		let match: ITajweed[] = [];
 		ayaSplited.forEach((txt, i) => {
 			if (IsChar(ayaSplited[i], [Maddah, MaddahShort])) {
+				const [prev] = GetPrevHuruf(ayaSplited, i);
 				const next = GetNext(ayaSplited, i);
 				if (IsChar(ayaSplited[next], [Hamzah, HamzahWau, HamzahYa])) {
 					match = [
 						...match,
 						{
 							class: 'madd-wajib',
-							start: i - 2,
+							start: prev,
 							end: next
 						}
 					];
