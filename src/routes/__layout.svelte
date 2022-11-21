3f602026430b11e46fa9939262018a55bc9a3113
falentio <riv.kevinfalent.io@gmail.com>
2022-11-21 20:51:51 +0700

    chore: remove unused code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
 	import '$lib/style/app.css';
-
-	import { Setting$ } from '$store/Setting';
-
-	// $: if (typeof window !== 'undefined') {
-	// 	const method = {
-	// 		dark: 'add',
-	// 		light: 'remove'
-	// 	};
-	// 	window.document.getElementsByTagName('html')[0].classList[method[$Setting$.theme]]('dark');
-	// }
-	
 </script>
 
-<div class="">
-	<slot />
-</div>
+<slot />
