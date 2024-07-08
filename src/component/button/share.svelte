4d81d79f8ffb5a9236277027d76b504c4d49e21c
Akhmad Salafudin <axmad386@gmail.com>
2024-07-08 11:07:28 +0700

    feat: native share

--- src/component/button/share.svelte
+++ src/component/button/share.svelte
@@ -4,6 +4,7 @@
 	import Icon from '@iconify/svelte';
 	export let text;
 	export let link = '';
+	export let title = '';
 
 	export let hashtags = '';
 	export let via = '';
@@ -38,6 +39,7 @@
 		};
 	});
 	$: {
+		if (typeof window != 'undefined') link = window.location.href;
 		url_whatsapp = encodeURI(`https://api.whatsapp.com/send/?text=${text}`);
 		url_telegram = encodeURI(`https://t.me/share/url?url=${link}&text=${text}`);
 		url_facebook = encodeURI(`https://web.facebook.com/sharer/sharer.php?u=${link}`);
@@ -45,11 +47,23 @@
 			`https://twitter.com/intent/tweet/?text=${text}&hashtags=${hashtags}&via=${via}&related=${related}&url=${link}`
 		);
 	}
+
+	const handleShare = async () => {
+		try {
+			await navigator.share({
+				url: link,
+				title,
+				text
+			});
+		} catch (_) {
+			show = !show;
+		}
+	};
 </script>
 
 <div class="relative" bind:this={menu}>
 	<div>
-		<button on:click={() => (show = !show)}>
+		<button on:click={handleShare}>
 			<Icon icon="cil:share-alt" color="#626262" width="24" height="24" />
 		</button>
 
@@ -61,7 +75,7 @@
 			rounded shadow-md flex space-y-3 items-center flex-col"
 			>
 				<a href={url_whatsapp} class="block">
-					<Icon icon="logos:whatsapp" width="24" height="24" />
+					<Icon icon="logos:whatsapp-icon" width="24" height="24" />
 				</a>
 				<a href={url_telegram}>
 					<Icon icon="bx:bxl-telegram" color="#40b3e0" width="24" height="24" />
